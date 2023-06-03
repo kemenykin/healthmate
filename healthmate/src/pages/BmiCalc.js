d7b5dbcb35a9d8152b636f7bea3d67f0ffc87e2e
kemenykin <kemenykin@gmail.com>
2023-06-03 15:19:21 +0200

    h3 commented

--- healthmate/src/pages/BmiCalc.js
+++ healthmate/src/pages/BmiCalc.js
@@ -5,7 +5,7 @@ const BmiCalc = () => {
 
     return (
         <div className='intro-text'>
-            <h3>BMI calculator</h3>
+            {/* <h3>BMI calculator</h3> */}
                 <p>This is where you can calculate BMI.</p>
                 <h4>At the Height, you have to separate the franctions of the number with DOT, NOT WITH comma. </h4>
                 <br/>

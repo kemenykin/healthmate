ff0ec1ae7895d7aeec0a0d1e484c9266bbe48207
kemenykin <kemenykin@gmail.com>
2023-06-04 17:15:06 +0200

    navbar styling

--- healthmate/src/components/Navigation.js
+++ healthmate/src/components/Navigation.js
@@ -5,14 +5,15 @@ import { FaUserAlt, FaHeart } from "react-icons/fa";
 const Navigation = () => {
     return (
         <div className="header-container">
+            <div className="logo-img" >
+                <Link to="/"><img src="https://imagizer.imageshack.com/img924/7773/ZwosXg.png" alt="" style={{ height: "30px" }} /></Link>
+            </div>
             <div className="navbar">
                 <Link to='/'>Home</Link>
                 <Link to='/bmi'> BMI calculator</Link>
                 <Link to='/daily'> Daily requirements</Link>
                 <Link to='/patients'> Patients</Link>
                 <Link to='/disclaimer'> Disclaimer</Link>
-            </div>
-            <div className="logo-img">
             </div>
             {/* <div className="nav-icons">
                 <Link to="#"><FaUserAlt /></Link>

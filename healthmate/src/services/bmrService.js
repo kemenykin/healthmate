98f27dbfe3a5d81fb2daaa136c747e545e121262
kemenykin <kemenykin@gmail.com>
2023-06-05 21:53:24 +0200

    bmr service logic cleanup

--- healthmate/src/services/bmrService.js
+++ healthmate/src/services/bmrService.js
@@ -1,9 +1,9 @@
 const bmrForMen = (weight, height, age) => {
-    return ((13.8 * (weight)) + (5.0 * height) - (6.8 * age) + 66.5); 
+    return (13.8 * weight + 5.0 * height - 6.8 * age + 66.5); 
 }
 
 const bmrForWomen = (weight, height, age) => {
-    return ((9.6 * weight) + (1.8 * height) - (4.7 * age) + 655.1);
+    return (9.6 * weight + 1.8 * height - 4.7 * age + 655.1);
 }
 
 export default {

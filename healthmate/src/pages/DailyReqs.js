e5ec83e2802bd62f04d456a3b7ebc611b13da3f5
kemenykin <kemenykin@gmail.com>
2023-06-05 21:53:55 +0200

    dailyreqs intro text changed

--- healthmate/src/pages/DailyReqs.js
+++ healthmate/src/pages/DailyReqs.js
@@ -4,9 +4,9 @@ import CalculateBMR from "../components/CalculateBMR";
 const DailyReqs = () => {
     return (
             <div className='daily-container'>
-                <p>This page is for calculate your daily needs eg. protein requirements, fluid intake etc.</p>
+                <p>You can make several metabolism calculations here.</p>
                 <div>
-                    <h3>Calculate BMR</h3>
+                    <h3>1. Calculate BMR</h3>
                     <CalculateBMR />
                 </div>
             </div>

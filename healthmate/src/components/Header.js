e0ecfb9d039687d37420203b15f198b986083207
kemenykin <kemenykin@gmail.com>
2023-06-05 19:23:16 +0200

    header txt added

--- healthmate/src/components/Header.js
+++ healthmate/src/components/Header.js
@@ -4,7 +4,14 @@ const Header = () => {
     return (
         <header>
             <div className="header-content">
+                <div className='header-text'>
+                    <h1>Help in need.</h1>
+                    <p>We are here and do the calculations. All you have to do is take your time &</p>
+                    <h3>educate your patient.</h3>
+                </div>
+                <div>
 
+                </div>
             </div>
         </header>
     )

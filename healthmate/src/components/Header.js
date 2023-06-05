ebf918cb8a5d6df5b2bfa87befa96f9be4309ac1
kemenykin <kemenykin@gmail.com>
2023-06-05 21:24:11 +0200

    header text theme classname added

--- healthmate/src/components/Header.js
+++ healthmate/src/components/Header.js
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
 import '../styles/header.css';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 const Header = () => {
+
+    const { theme } = useContext(ThemeContext);
+
     return (
         <header>
             <div className="header-content">
-                <div className='header-text'>
+                <div className={`header-text ${theme}`}>
                     <h1>Help in need.</h1>
                     <p>We are here and do the calculations. All you have to do is take your time &</p>
                     <h3>educate your patient.</h3>

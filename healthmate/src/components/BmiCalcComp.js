c868866df4331b3cec186d131599181e44075512
kemenykin <kemenykin@gmail.com>
2023-06-05 18:46:26 +0200

    bmi comp css fixing

--- healthmate/src/components/BmiCalcComp.js
+++ healthmate/src/components/BmiCalcComp.js
@@ -28,28 +28,28 @@ const BmiCalcComp = () => {
     }
 
     return (
-        <>
-            <div className='bmi-calculator'>
-                <form onSubmit={calculate}>
-                    <label htmlFor="weight">Weight</label>
-                    <input type="text" value={weight} onChange={handleWeightChange} id="weight" name="weight" placeholder="in kilogramms" />
-                    <label htmlFor="height">Height</label>
-                    <input type="text" value={height} onChange={handleHeightChange} id="height" name="height" placeholder="in metres" />
-                    <button type='submit'>Calculate</button>
-                </form>
+            <div className='bmi-wrap'>
+                <div className='bmi-calculator'>
+                    <form onSubmit={calculate}>
+                        <label htmlFor="weight">Weight</label>
+                        <input type="text" value={weight} onChange={handleWeightChange} id="weight" name="weight" placeholder="in kilogramms" />
+                        <label htmlFor="height">Height</label>
+                        <input type="text" value={height} onChange={handleHeightChange} id="height" name="height" placeholder="in metres" />
+                        <button type='submit'>Calculate</button>
+                    </form>
+                </div>
+                <div className="display-bmi">
+                    <h4>Your patient's Body Mass Index is: </h4>
+                    <p className="calculated-bmi">{bmi}</p>
+                </div>
+                <div>
+                    <p>Weight and height are used to calculate the BMI, which is used to classify subject as underweight, normal weight, overweight or obese.</p>
+                    <p>The table below provides the WHO cutoff points for adults.</p>
+                </div>
+                <div className="bmi-table">
+                    <img src="https://www.ncbi.nlm.nih.gov/books/NBK551660/bin/bmi__WHO.jpg" width="400px" alt='' />
+                </div>
             </div>
-            <div className="display-bmi">
-                <h4>Your patient's Body Mass Index is: </h4>
-                <p className="calculated-bmi">{bmi}</p>
-            </div>
-            <div>
-                <p>Weight and height are used to calculate the BMI, which is used to classify subject as underweight, normal weight, overweight or obese.</p> 
-                <p>The table below provides the WHO cutoff points for adults.</p>
-            </div>
-            <div className="bmi-table">
-                <img src="https://www.ncbi.nlm.nih.gov/books/NBK551660/bin/bmi__WHO.jpg" width="400px" alt='' />
-            </div>
-        </>
     )
 }
 

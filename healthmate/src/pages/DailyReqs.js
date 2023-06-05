2ef1902f4730d80e8ca1d2dacbd60e67c6702cd2
kemenykin <kemenykin@gmail.com>
2023-06-05 22:20:24 +0200

    css formatting

--- healthmate/src/pages/DailyReqs.js
+++ healthmate/src/pages/DailyReqs.js
@@ -4,10 +4,10 @@ import CalculateBMR from "../components/CalculateBMR";
 const DailyReqs = () => {
     return (
             <div className='daily-container'>
-                <p>You can make several metabolism calculations here.</p>
                 <div>
                     <h3>1. Calculate BMR</h3>
                     <p>with the Harris & Benedict equation (1918)</p>
+                    <br />
                     <CalculateBMR />
                 </div>
             </div>

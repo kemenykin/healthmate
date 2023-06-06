362060ad0cab5547eaecddaa39d5bd26806cb250
kemenykin <kemenykin@gmail.com>
2023-06-06 09:21:39 +0200

    select dropdown done

--- healthmate/src/pages/DailyReqs.js
+++ healthmate/src/pages/DailyReqs.js
@@ -1,17 +1,24 @@
 import '../styles/dailyreqs.css';
 import CalculateBMR from "../components/CalculateBMR";
+import ProteinReqsComp from '../components/ProteinReqsComp';
 
 const DailyReqs = () => {
     return (
-            <div className='daily-container'>
-                <div className='bmr-calc-container'>
-                    <h3>1. Calculate BMR</h3>
-                    <p>with the Harris & Benedict equation (1918)</p>
-                    <br />
-                    <CalculateBMR />
-                    <br />
-                </div>
+        <div className='daily-container'>
+            <div className='bmr-calc-container'>
+                <h3>1. Calculate BMR</h3>
+                <p>with the Harris & Benedict equation (1918)</p>
+                <br />
+                <CalculateBMR />
+                <br />
             </div>
+            <div className='proteinreqs-container'>
+                <h3>2. Protein requirements</h3>
+                <p>in different age and patient groups</p>
+                <br />
+                <ProteinReqsComp />
+            </div>
+        </div>
     )
 }
 

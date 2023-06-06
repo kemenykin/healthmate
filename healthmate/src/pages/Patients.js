26e7ef94c517b2ce00e0c8b85717b76834bc8fd4
kemenykin <kemenykin@gmail.com>
2023-06-06 19:58:08 +0200

    fixing css

--- healthmate/src/pages/Patients.js
+++ healthmate/src/pages/Patients.js
@@ -1,9 +1,11 @@
+import "../styles/patients.css";
+
 const Patients = () => {
     return (
-        <div>
-            <div>
-                <h3>Patients</h3>
-                <p>You can see your patient's list here. List it, add new patient, maybe delete and after that, see the updated list.</p>
+        <div className="patients-container">
+            <div className="intro-patients">
+                <h3>Soon you can see your patient's list here. List it, add new patient, maybe delete and after that, see the updated list.</h3>
+                <p>Work in progress, under construction.</p>
             </div>
         </div>
     )

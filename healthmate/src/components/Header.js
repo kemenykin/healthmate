d621e57f68b8870ee1337bc1cbb914145daeef30
kemenykin <kemenykin@gmail.com>
2023-06-08 16:43:03 +0200

    changed header to responsive & new img

--- healthmate/src/components/Header.js
+++ healthmate/src/components/Header.js
@@ -4,15 +4,15 @@ import { ThemeContext } from '../contexts/ThemeContext';
 
 const Header = () => {
 
-    const { theme } = useContext(ThemeContext);
+    // const { theme } = useContext(ThemeContext);
 
     return (
         <header>
             <div className="header-content">
-                <div className={`header-text ${theme}`}>
+                <div className="header-text">
                     <h1>Help in need.</h1>
-                    <p>We are here and do the calculations. All you have to do is take your time &</p>
-                    <h3>educate your patient.</h3>
+                    <div className='text-div'>We are here and do the calculations. All you have to do is
+                    take your time & <b>educate your patient.</b></div>
                 </div>
                 <div>
 

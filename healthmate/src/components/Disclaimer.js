b778b3ceaea5a35b2c6d27da00eb21638560961f
kemenykin <kemenykin@gmail.com>
2023-06-09 00:22:21 +0200

    temporary disclaimers done

--- healthmate/src/components/Disclaimer.js
+++ healthmate/src/components/Disclaimer.js
@@ -4,11 +4,15 @@ const Disclaimer = () => {
     return (
         <div className='disclaimer-wrap'>
             <div className='disclaimer'>
-                <div className='description'>
-                    <h3>Disclaimer</h3>
-                    <p>Keep in mind:</p>
-                    <p>These calculations are based on general formulas and may not be accurate for everyone. For example, the formula for calculating daily calorie needs is based on average values and doesn't take into account factors such as individual metabolism, activity level, and body composition. So it provides some information, but because of the of the limitations of these calculations we encourage you to consult with a healthcare professional or registered dietitian for personalized advice.</p>
+                <div>
+                    <img src="https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 </div>
+                <div className='description'>
+                    <h4>Keep in mind:</h4>
+                    <p>These calculations are based on general formulas and may not be accurate for everyone. For example, the formula for calculating daily calorie needs is based on average values and doesn't take into account factors such as individual metabolism, activity level, and body composition. So it provides some information, but because of the of the limitations of these calculations we encourage you to consult with a healthcare professional or registered dietitian for personalized advice.</p>
+                <br />
+                </div>
+
             </div>
         </div>
     )

5954d9bc5a14d848947476e9c82e2edd75f28e12
kemenykin <kemenykin@gmail.com>
2023-06-09 00:05:44 +0200

    patients info

--- healthmate/src/pages/Patients.js
+++ healthmate/src/pages/Patients.js
@@ -4,8 +4,14 @@ const Patients = () => {
     return (
         <div className="patients-container">
             <div className="intro-patients">
-                <h3>Soon you can see your patient's list here. List it, add new patient, maybe delete and after that, see the updated list.</h3>
-                <p>Work in progress, under construction.</p>
+                <div className="description">
+                    <h3>Soon you can see your patient's list here. List it, add new patient, maybe delete and after that, see the updated list.</h3>
+                    <p>Work in progress, under construction.</p>
+                    <p>According to our plans, you can create individual profiles, patient's list. You can track their therapies/recommendations, and take notes in connection with the dietotherapy.</p>
+                </div>
+                <div>
+                    <img src="https://images.pexels.com/photos/7261097/pexels-photo-7261097.jpeg" alt=""/>
+                </div>
             </div>
         </div>
     )

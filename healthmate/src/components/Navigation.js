71f2db66e00071ec17969d2c5fe9935d9c648ebb
kemenykin <kemenykin@gmail.com>
2023-06-14 16:19:19 +0200

    navbar hamburger logic

--- healthmate/src/components/Navigation.js
+++ healthmate/src/components/Navigation.js
@@ -1,21 +1,31 @@
 import { Link } from "react-router-dom";
 import '../styles/navigation.css';
 import { FaUserAlt, FaHeart, FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Navigation = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    console.log(isMenuOpen);
+
     return (
         <div className="header-container">
             <div className="logo-img" >
                 <Link to="/"><img src="https://imagizer.imageshack.com/img924/7773/ZwosXg.png" alt="" style={{ height: "50px" }} /></Link>
             </div>
-            <div className="navbar">
+            <div className={`navbar ${isMenuOpen ? 'visible' : ''}`}>
                 <Link to='/bmi' className="navbar-links"> BMI calculator</Link>
                 <Link to='/daily' className="navbar-links"> Daily requirements</Link>
                 <Link to='/patients' className="navbar-links"> Patients</Link>
                 <Link to='/disclaimer' className="navbar-links"> Disclaimer</Link>
             </div>
             <div className="navbar-toggle">
-                <Link><FaBars /></Link>
+                <Link><FaBars className={`navbar-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}/></Link>
             </div>
         </div>
     )

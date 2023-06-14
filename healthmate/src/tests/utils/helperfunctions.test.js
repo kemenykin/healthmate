bb8ec4eb27441886b8d16b6d86a7c8868e82bac3
kemenykin <kemenykin@gmail.com>
2023-06-14 14:49:04 +0200

    first jest test

--- healthmate/src/tests/utils/helperfunctions.test.js
+++ healthmate/src/tests/utils/helperfunctions.test.js
@@ -1,5 +1,10 @@
 import calculateBmi from "../../services/bmi-service";
+import estimateFluid from "../../services/fluidService";
 
 test('divides 56 with 1.68 * 1.68', () => {
     expect(calculateBmi(56, 1.68)).toBe(19.84);
   });
+
+test('multiply 60 and 35', () => {
+    expect(estimateFluid(60, 35)).toBe(2100);
+});  
